e11eaed629d8de29a0ca7ad2d8d2e6575306c5c9
yoel11705 <mryexs117@gmail.com>
2026-01-28 13:51:31 -0600

    conexion

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  
+    port: 5173,
+    proxy: {
+      '/uploads': 'http://localhost:5000',
+      '/videos': 'http://localhost:5000', 
+      
+    }
+  }
 })
